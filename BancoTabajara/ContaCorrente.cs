02e012ad6e4e7576208dc035fff3162bffc6c5f9
joicencm <joicencm@gmail.com>
2025-04-22 22:00:18 -0300

    Implementação Classe ContaCorrente - Função/Metodo Sacar

---
+++ BancoTabajara/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿namespace BancoTabajara
+{
+    public class ContaCorrente
+    {
+        public int numero;
+        public double saldo;
+        public double limite;
+
+        public double Sacar(double valor)
+        {
+            saldo -= valor;
+            Console.WriteLine($"Saque de R${valor} realizado com sucesso. Saldo atual de R$ {saldo}");
+            return valor;
+        }
+    }
+}
--- BancoTabajara/Program.cs
+++ BancoTabajara/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.saldo = 3500;
+            conta1.numero = 10310;
+            conta1.limite = 0;
             
+
+            conta1.Sacar(300);
+
+            Console.ReadLine();
         }
     }
 }
