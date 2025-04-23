d6f840dd16a495a5e860f3132bdbb0e24c287dac
joicencm <joicencm@gmail.com>
2025-04-23 20:03:19 -0300

    Implementação Função/Metodo Transferencia entre Conta e o Metodo Registrar Movimentação

--- BancoTabajara/ContaCorrente.cs
+++ BancoTabajara/ContaCorrente.cs
@@ -1,10 +1,25 @@
-﻿namespace BancoTabajara
+﻿
+namespace BancoTabajara
 {
+    public class Movimentacao
+    {
+        public double Valor;
+        public string Tipo;
+
+        public Movimentacao(double valor, string tipo)
+        {
+            Valor = valor;
+            Tipo = tipo;
+        }
+    }
+
     public class ContaCorrente
     {
         public int numero;
         public double saldo;
         public double limite;
+        public Movimentacao[] movimentacoes;
+        public int contadorMovimentacoes = 0;
 
         public void Sacar(double valor)
         {
@@ -12,12 +27,32 @@
                 Console.WriteLine("Saldo insulficiente");
 
             saldo -= valor;
+            RegistrarMovimentacao(new Movimentacao(valor, "Débito"));
+        }
+
+        public void RegistrarMovimentacao(Movimentacao movimentacao)
+        {
+            movimentacoes[contadorMovimentacoes++] = movimentacao;
         }
 
         public void Depositar(double valor)
         {
             saldo += valor;
         }
-        public void ConsultarSaldo
+
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"Saldo atual: R$ {saldo}");
+        }
+
+        public void transferirPara(ContaCorrente destino, int valor)
+        {
+            if (valor > saldo + limite)
+                Console.WriteLine("Saldo insulficiente, ou ultrapassou o limite");
+
+            this.Sacar(valor);
+            destino.Depositar(valor);
+            Console.WriteLine($"Transferência de R${valor} realizada para a conta {destino.numero}.");
+        }
     }
 }
--- BancoTabajara/Program.cs
+++ BancoTabajara/Program.cs
@@ -8,10 +8,21 @@
             conta1.saldo = 3500;
             conta1.numero = 10310;
             conta1.limite = 0;
+            conta1.movimentacoes = new Movimentacao[10];
             
 
             conta1.Sacar(300);
             conta1.Depositar(500);
+            conta1.ConsultarSaldo();
+
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.saldo = 1500;
+            conta2.numero = 10311;
+            conta2.limite = 0;
+            conta2.movimentacoes = new Movimentacao[10];
+
+            conta1.transferirPara(conta2, 501);
+            conta2.ConsultarSaldo();
 
             Console.ReadLine();
         }
